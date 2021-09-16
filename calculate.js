f3d7b907907dc854852e5f8b42a3e66d3acd3da1
vinnivm <vineeth@mca.ajce.in>
2021-09-16 10:53:57 +0530

    scripts changed to js file

---
+++ calculate.js
@@ -0,0 +1,40 @@
+ function calculatedate()
+                {
+                        let inputdate=document.getElementById("date1id").value;
+                        let holiday=moment(document.getElementById("holidayid").value).format("DD/MM/YYYY");
+                        let nod=document.getElementById("nod1").value;
+                        let inputdatetemp;
+                        let day;
+                        for ( let i=1 ;i<=nod ; i++)
+                        {
+                            inputdatetemp=moment(inputdate).add(i,'day').format("DD/MM/YYYY");
+                            if( holiday == inputdatetemp )
+                                {
+                                    nod=parseInt(nod)+1;
+                                    i++;
+                                }
+                            day=moment(inputdate).add(i,'day').format('dddd');
+                            if( day == "Saturday" )
+                                nod=parseInt(nod)+1;
+                            else if( day == "Sunday" )
+                                nod=parseInt(nod)+1;                                         
+                        }
+                        let newdate=moment(inputdate).add(nod,'day').format("DD/MM/YYYY");
+                        document.getElementById("outputid1").value="Date: "+newdate+" , "+day;
+                }
+                function calculatedate1()
+                {
+                    var xhttp = new XMLHttpRequest();
+                    let inputdate=document.getElementById("date1id").value;
+                    let holiday=document.getElementById("holidayid").value;
+                    let nod=document.getElementById("nod1").value;
+                    xhttp.onreadystatechange = function() {
+                    console.log(this);
+                    if (this.readyState == 4 && this.status == 200)
+                    {
+                        console.log(this.responseText)
+                    }
+                    };
+                    xhttp.open("GET", "senddatatoserver.java?inputdate="+ inputdate +"& holiday="+ holiday +"& nod="+ nod, true);
+                    xhttp.send();
+                }
